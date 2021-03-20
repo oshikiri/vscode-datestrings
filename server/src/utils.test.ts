0d8bb8b5fdcb0bc689b50964b9701efe6583d8d7
Takamasa Oshikiri <mail@oshikiri.org>
2021-03-20 23:20:09 +0900

    Refactor utils.test

--- server/src/utils.test.ts
+++ server/src/utils.test.ts
@@ -1,20 +1,24 @@
 import { strict as assert } from "assert";
 import * as utils from "./utils";
 
-context("range(2, 4)", () => {
-  it("returns -1, ... 3", () => {
-    assert.deepEqual(utils.range(2, 4), [-1, 0, 1, 2, 3]);
-  });
-});
+context("utils", () => {
+  context("#range", () => {
+    context("when (2, 4)", () => {
+      it("returns -1, ... 3", () => {
+        assert.deepEqual(utils.range(2, 4), [-1, 0, 1, 2, 3]);
+      });
+    });
 
-context("range(1, 4)", () => {
-  it("returns 0, ..., 3", () => {
-    assert.deepEqual(utils.range(1, 4), [0, 1, 2, 3]);
-  });
-});
+    context("when (1, 4)", () => {
+      it("returns 0, ..., 3", () => {
+        assert.deepEqual(utils.range(1, 4), [0, 1, 2, 3]);
+      });
+    });
 
-context.skip("range(-1, 1)", () => {
-  it("throws", () => {
-    assert.throws(() => utils.range(-1, 1));
+    context.skip("when (-1, 1)", () => {
+      it("throws", () => {
+        assert.throws(() => utils.range(-1, 1));
+      });
+    });
   });
 });
