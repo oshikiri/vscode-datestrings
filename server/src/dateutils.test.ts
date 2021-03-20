4c56a0699bcd458886418101b40839774b05c484
Takamasa Oshikiri <mail@oshikiri.org>
2021-03-20 23:36:23 +0900

    Fix dateDiffString implementation (#3)
    * Fix dateDiffString implementation
    
    * Fix completion e2e test

--- server/src/dateutils.test.ts
+++ server/src/dateutils.test.ts
@@ -4,9 +4,9 @@ import * as dateutils from "./dateutils";
 context("dateutils", () => {
   it("#dateDiffString", () => {
     assert.equal(dateutils.dateDiffString(-2), "2 days ago");
-    assert.equal(dateutils.dateDiffString(-1), "1 days ago");
+    assert.equal(dateutils.dateDiffString(-1), "yesterday");
     assert.equal(dateutils.dateDiffString(0), "today");
-    assert.equal(dateutils.dateDiffString(1), "1 days after");
+    assert.equal(dateutils.dateDiffString(1), "tomorrow");
     assert.equal(dateutils.dateDiffString(2), "2 days after");
   });
 });
