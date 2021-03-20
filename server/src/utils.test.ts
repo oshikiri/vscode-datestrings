347c809f6fee85493a6004fd384e969d2bb8487b
Takamasa Oshikiri <mail@oshikiri.org>
2021-03-20 23:49:29 +0900

    Return [from, to] instead of (from, to)

--- server/src/utils.test.ts
+++ server/src/utils.test.ts
@@ -5,13 +5,13 @@ context("utils", () => {
   context("#range", () => {
     context("when (2, 4)", () => {
       it("returns -1, ... 3", () => {
-        assert.deepEqual(utils.range(2, 4), [-1, 0, 1, 2, 3]);
+        assert.deepEqual(utils.range(1, 3), [-1, 0, 1, 2, 3]);
       });
     });
 
     context("when (1, 4)", () => {
       it("returns 0, ..., 3", () => {
-        assert.deepEqual(utils.range(1, 4), [0, 1, 2, 3]);
+        assert.deepEqual(utils.range(0, 3), [0, 1, 2, 3]);
       });
     });
 
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -4,10 +4,10 @@ export function range(from: number, to: number): number[] {
   assert.ok(from >= 0, "Argument 'from' is negative");
   assert.ok(to >= 0, "Argument 'to' is negative");
 
-  const negatives = Array.from(Array(from).keys())
+  const negatives = Array.from(Array(from + 1).keys())
     .slice(1)
     .map((x) => -x)
     .reverse();
-  const positives = Array.from(Array(to).keys()).slice(1);
+  const positives = Array.from(Array(to + 1).keys()).slice(1);
   return [...negatives, 0, ...positives];
 }
