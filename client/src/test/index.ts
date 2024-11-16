9686c84b154666fa8b1781d348cba8a7015ce1a9
Takamasa Oshikiri <mail@oshikiri.org>
2024-11-16 19:28:47 +0900

    Add glob

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import * as Mocha from "mocha";
-import { glob } from "fs";
+import * as glob from "glob";
 
 export function run(): Promise<void> {
   // Create the mocha test
