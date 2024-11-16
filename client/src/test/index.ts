d0a67673bfebaf93b0df01c171e95aeadbe8b7c1
Takamasa Oshikiri <mail@oshikiri.org>
2024-11-16 19:37:13 +0900

    async-await

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import * as Mocha from "mocha";
 import * as glob from "glob";
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: "bdd",
@@ -12,24 +12,16 @@ export function run(): Promise<void> {
 
   const testsRoot = __dirname;
 
-  return new Promise((resolve, reject) => {
-    glob.glob("**.test.js", { cwd: testsRoot }).then((files) => {
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+  const files: string[] = await glob.glob("**.test.js", { cwd: testsRoot });
 
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`));
-          } else {
-            resolve();
-          }
-        });
-      } catch (err) {
-        console.error(err);
-        reject(err);
-      }
-    });
+  // Add files to the test suite
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+  mocha.run((failures) => {
+    if (failures > 0) {
+      return new Error(`${failures} tests failed.`);
+    }
   });
+
+  return;
 }
